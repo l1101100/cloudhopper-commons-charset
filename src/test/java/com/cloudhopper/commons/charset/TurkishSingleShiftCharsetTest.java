ed8f75739a7bfeb90807db1fdfa97b77b58b745b
l1101100 <muamer.bajrich@gmail.com>
2016-10-12 12:07:59 +0200

    turkish single shift test

--- src/test/java/com/cloudhopper/commons/charset/TurkishSingleShiftCharsetTest.java
+++ src/test/java/com/cloudhopper/commons/charset/TurkishSingleShiftCharsetTest.java
@@ -45,5 +45,25 @@ public class TurkishSingleShiftCharsetTest {
 
         // gsm char Æ
         Assert.assertEquals(true, TurkishSingleShiftCharset.canRepresent("Æ"));
+
+        String text160 = "Aa Üü Ğğ iİ Şş Çç Öö ıI";
+
+        TurkishSingleShiftCharset tssc = new TurkishSingleShiftCharset();
+        byte[] textBytes = tssc.encode(text160);
+
+        System.out.print(bytesToHex(textBytes));
+
+    }
+
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
     }
 }
